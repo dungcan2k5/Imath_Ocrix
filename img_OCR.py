eeb6a3d40548637ea274bf73b8d62d46fd4d976c
dungcan2k5 <trannamnghiemtuc@gmail.com>
2022-12-03 23:17:18 +0700

    Create 2 windows application

--- img_OCR.py
+++ img_OCR.py
@@ -4,30 +4,44 @@ from PIL import Image, ImageTk
 from app import App
 from define import *
 from img_OCR_func import *
+from math_OCR import ScrMath
 
 # Splash Window
 splash_root = Tk()
 splash_root.geometry("300x200")
 splash_label = Label(text="Splash Screen!", font=("Helvetica", 18)).pack(pady=20)
 
+def run():
+    main_window = Toplevel
+    ScrImg(main_window)
+
+
 # Main Window
-def main_window():
-    splash_root.destroy()
+class ScrImg():
+    def __init__(self, window) -> None:
+        splash_root.destroy()
 
-    window = Tk()
-    App(window)
+        # Show Math_OCR Screen
+        def openMathScr():
+            math_scr = Toplevel
+            ScrMath(math_scr)
+            window.destroy()
 
-    # Chọn Loại Chương Trình
-    choose_ImgOCR = Button(text="Image OCR     |", bg=COLOR_BACKGROUND,         
-        borderwidth=0, activebackground=COLOR_BACKGROUND)
-    choose_ImgOCR.grid(column=0, row=0, sticky=NE, pady=10)
+        window = Tk()
+        App(window)
 
-    choose_MathOCR = Button(text="   Math OCR", bg=COLOR_BACKGROUND, 
-        borderwidth=0, activebackground=COLOR_BACKGROUND)
-    choose_MathOCR.grid(column=1, row=0, sticky=NW, pady=10)
+        # Chọn Loại Chương Trình
+        choose_ImgOCR = Button(window, text="Image OCR     |", bg=COLOR_BACKGROUND,borderwidth=0, activebackground=COLOR_BACKGROUND)
+        choose_ImgOCR.grid(column=0, row=0, sticky=NE, pady=10)
 
-    window.columnconfigure(0, weight=1)
-    window.columnconfigure(1, weight=1)                     
+        choose_MathOCR = Button(window, text="   Math OCR", bg=COLOR_BACKGROUND, 
+            borderwidth=0, activebackground=COLOR_BACKGROUND, command=openMathScr)
+        choose_MathOCR.grid(column=1, row=0, sticky=NW, pady=10)
 
-splash_root.after(3000, main_window)
+        Label(window, text="Đây là trang 1").grid(row=1)
+
+        window.columnconfigure(0, weight=1)
+        window.columnconfigure(1, weight=1) 
+    
+splash_root.after(3000, run)
 mainloop()
--- math_OCR.py
+++ math_OCR.py
@@ -4,19 +4,26 @@ from PIL import Image, ImageTk
 from app import App
 from define import *
 
-window = Tk()
-App(window)
 
-# Chọn Loại Chương Trình
-choose_ImgOCR = Button(text="Image OCR     |", bg=COLOR_BACKGROUND,         
-    borderwidth=0, activebackground=COLOR_BACKGROUND)
-choose_ImgOCR.grid(column=0, row=0, sticky=NE, pady=10)
+class ScrMath():
+    def __init__(self, window) -> None:
 
-choose_MathOCR = Button(text="   Math OCR", bg=COLOR_BACKGROUND, 
-    borderwidth=0, activebackground=COLOR_BACKGROUND)
-choose_MathOCR.grid(column=1, row=0, sticky=NW, pady=10)
+        # Open Img_OCR Screen
+        def openImgScr():
+            window.destroy()
 
-window.columnconfigure(0, weight=1)
-window.columnconfigure(1, weight=1)
+        window = Tk()
+        App(window)
+        
+        # Chọn Loại Chương Trình
+        choose_ImgOCR = Button(window, text="Image OCR     |", bg=COLOR_BACKGROUND, borderwidth=0, activebackground=COLOR_BACKGROUND, command=openImgScr)
+        choose_ImgOCR.grid(column=0, row=0, sticky=NE, pady=10)
 
-mainloop()
+        choose_MathOCR = Button(window, text="   Math OCR", bg=COLOR_BACKGROUND, 
+            borderwidth=0, activebackground=COLOR_BACKGROUND)
+        choose_MathOCR.grid(column=1, row=0, sticky=NW, pady=10)
+
+        Label(window, text="Đây là trang 2").grid(row=1)
+
+        window.columnconfigure(0, weight=1)
+        window.columnconfigure(1, weight=1)
