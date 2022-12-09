e22d947ed327daea8643a796536a9ef3ec3603bb
dungcan2k5 <trannamnghiemtuc@gmail.com>
2022-12-09 09:40:52 +0700

    img_OCR Successful

--- img_OCR.py
+++ img_OCR.py
@@ -2,7 +2,7 @@ from tkinter import *
 from PIL import Image, ImageTk
 from io import BytesIO
 import requests
-import time
+import pyperclip
 
 
 from define import *
@@ -16,66 +16,102 @@ class ScrImg(Frame):
         self["bg"] = COLOR_BACKGROUND
         self.pack(padx = 10, pady = 10)
 
+        # Resize Image
+        def resizeImg(option, uri):
+            if option == "uri":
+                # Get Image
+                response = requests.get(uri)
+                im = Image.open(BytesIO(response.content))
+                height = im.height
+                width = im.width
+
+                # Resize Image
+                if im.width > 425:
+                    height = round(410 * (im.height / im.width))
+                    width = 410
+                
+                return im.resize((width, height), Image.Resampling.LANCZOS)
+            elif option == "image":
+                # Open image
+                im = Image.open(f"Img_temp/{file_name}.png")
+                height = im.height
+                width = im.width
+
+                # Resize Image
+                if im.width > 425:
+                    height = round(410 * (im.height / im.width))
+                    width = 410
+
+                return im.resize((width, height), Image.Resampling.LANCZOS)
+
+        # Input Uri of Image
         def inputUriBox():
 
             UrlInputBox = Toplevel()
             content = Entry(UrlInputBox)
             content.pack()
-            uri = content.get()
-            while(len(uri) == 0):
-                time.sleep(1.5)
+            
+            # Show Text of Image
+            def click():
                 uri = content.get()
 
-            def click():
-               UrlInputBox.distroy()
+                # Detect text from uri
+                text = detect_text_uri(uri)
+                showTextLabel.config(state="normal")
+                showTextLabel.delete('1.0', END)
+                showTextLabel.insert("end", text)
+                showTextLabel.config(state="disabled")
                 
+                # Resize Image
+                resized = resizeImg("uri", uri)
+                imgUri = ImageTk.PhotoImage(resized) 
 
+                # Show Image
+                showImgLabel.config(state="normal")
+                showImgLabel.delete('1.0', END)
+                showImgLabel.image_create(END, image=imgUri)
+                showImgLabel.image = imgUri
+                showImgLabel.config(state="disabled")
+
+                UrlInputBox.destroy()
+                
             Button(UrlInputBox, text="OK", command=click).pack()
-            
-            text = detect_text_uri(uri)
-            showTextLabel.config(state="normal")
-            showTextLabel.delete('1.0', END)
-            showTextLabel.insert("end", text)
-            showTextLabel.config(state="disabled")
+        
+        # copy selected text to clipboard
+        def copyText(): 
+            showTextLabel.tag_add("sel", "1.0","end")
+            showTextLabel.tag_config("sel",background="black",foreground="white")
+            content = showTextLabel.selection_get()
+            pyperclip.copy(content)
 
-            response = requests.get(uri)
-            img = Image.open(BytesIO(response.content))
-            height = round(425 * (img.height / img.width))
-            resized = img.resize((425, height), Image.Resampling.LANCZOS)
-            imgUri = ImageTk.PhotoImage(resized) 
-
-            showImgLabel.config(state="normal")
-            showImgLabel.delete('1.0', END)
-            showImgLabel.image_create(END, image=imgUri)
-            showImgLabel.image = imgUri
-            showImgLabel.config(state="disabled")           
-
+        # Show text detected from Image
         def showText():
+            # Detect text from image
             text = detect_text()
             showTextLabel.config(state="normal")
             showTextLabel.delete('1.0', END)
             showTextLabel.insert("end", text)
             showTextLabel.config(state="disabled")
 
-            # Image is Captured
-            im = Image.open(f"Img_temp/{file_name}.png")
-            height = round(425 * (im.height / im.width))
-            resized = im.resize((425, height), Image.Resampling.LANCZOS)
+            # Resize Image
+            resized = resizeImg("image", None)
             imgCapScr = ImageTk.PhotoImage(resized)
 
+            # Show Image
             showImgLabel.config(state="normal")
             showImgLabel.delete('1.0', END)
             showImgLabel.image_create(END, image=imgCapScr)
             showImgLabel.image = imgCapScr
             showImgLabel.config(state="disabled")
 
-        # Show Image
-        showImgLabel = Text(self, bg="light yellow", height=20)
-        showImgLabel.grid(columnspan=2, row=0, sticky=NSEW, padx=10)
+        # Show Image Label
+        showImgLabel = Text(self, bg="light yellow", height=20, font=("Times New Roman", 11))
+        showImgLabel.grid(columnspan=2, row=0, padx=10)
 
-        # Show Text
-        showTextLabel = Text(self, bg="white", height=20)
-        showTextLabel.grid(column=2, row=0, sticky=NSEW, padx=10)
+        # Show Text Label
+        global showTextLabel
+        showTextLabel = Text(self, bg="white", height=20, font=("Times New Roman", 11))
+        showTextLabel.grid(column=2, row=0, padx=10)
 
         # Icon For URL Button
         icoURL = ImageTk.PhotoImage(Image.open("icon/icon_url.png").resize((30, 30), Image.Resampling.LANCZOS))
@@ -97,10 +133,9 @@ class ScrImg(Frame):
         icoClipboard = ImageTk.PhotoImage(Image.open("icon/icon_clipboard.png").resize((30, 30), Image.Resampling.LANCZOS))
 
         # Copy Content To Clipboard
-        btnCopy = Button(self, image=icoClipboard, text="Sao chép vào bộ nhớ tạm", width=190, height=40, compound="left", command=showText)
+        btnCopy = Button(self, image=icoClipboard, text="Sao chép vào bộ nhớ tạm", width=190, height=40, compound="left", command=copyText)
         btnCopy.image = icoClipboard
         btnCopy.grid(column=2, row=1, pady=10)
-
 
         # Thiết đặt kích thước cách cột trong khung hinh
         self.columnconfigure(0, weight=1)
--- math_OCR.py
+++ math_OCR.py
@@ -1,15 +1,52 @@
 from tkinter import *
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageGrab
+import time, pyperclip
 
 from app import App
 from define import *
+from snip_screen import snip_screen
+from snip_screen import file_name
 
 class ScrMath(Frame):
     def __init__(self, parent):
         super().__init__(parent)
         parent["bg"] = COLOR_BACKGROUND
-        
-        Label(self, text="Đây là trang 2").grid(row=1)
-
+        self["bg"] = COLOR_BACKGROUND
         self.pack(padx = 10, pady = 10)
+
+        def show_image():
+            pass
+
+
+        # Show Image
+        showImgLabel = Text(self, bg="light yellow", height=6)
+        showImgLabel.grid(columnspan=2, row=0, sticky=NSEW, pady=10)
+
+        # Show Text
+        showTextLabel = Text(self, bg="white", height=6)
+        showTextLabel.grid(columnspan=2, row=1, sticky=NSEW, pady=10)
+
+        # Show Latex
+        showLatexLabel = Text(self, bg="white", height=1, font=("Arial", 14))
+        showLatexLabel.grid(columnspan=2, row=2, sticky=NSEW, pady=10)
+
+        # Icon For Snip Screen Button
+        icoSnipScr = ImageTk.PhotoImage(Image.open("icon/snip_screen.png").resize((30, 30), Image.Resampling.LANCZOS))
+        
+        # Snip Screen Button
+        btnSnipScr = Button(self, image=icoSnipScr, text="Chụp ảnh màn hình", width=190, height=40, compound="left", command=show_image)
+        btnSnipScr.image = icoSnipScr
+        btnSnipScr.grid(column=0, row=3, pady=10)
+
+        # Icon Copy To Clipboard
+        icoClipboard = ImageTk.PhotoImage(Image.open("icon/icon_clipboard.png").resize((30, 30), Image.Resampling.LANCZOS))
+
+        # Copy Content To Clipboard
+        btnCopy = Button(self, image=icoClipboard, text="Sao chép vào bộ nhớ tạm", width=190, height=40, compound="left")
+        btnCopy.image = icoClipboard
+        btnCopy.grid(column=1, row=3, pady=10)        
+
+        # Thiết đặt kích thước cách cột trong khung hinh
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
 
